ea3fe6ff0a47fd98b7b5176d0de720dffde0d692
Lubos Jurca <lubos.jurca@gmail.com>
2022-10-10 15:33:53 +0200

    Basic skeleton

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// // generator hesla
+// let solutionEl = document.getElementById("solution")
+
+
+// function generatePassword(){
+//     for(let i = 0; i < 16; i++){
+//     let randomIndex = Math.floor(Math.random() * characters.length)
+//     solutionEl.innerText += characters[randomIndex]
+//      }
+
+// }
+
+// generatePassword()
