983460345da60f2c1b11afb65835e7b570cf36b3
Lubos Jurca <lubos.jurca@gmail.com>
2022-10-10 20:25:01 +0200

    beta

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 // // generator hesla
-// let solutionEl = document.getElementById("solution")
 
 
-// function generatePassword(){
+// function generate(){
 //     for(let i = 0; i < 16; i++){
 //     let randomIndex = Math.floor(Math.random() * characters.length)
 //     solutionEl.innerText += characters[randomIndex]
@@ -11,4 +9,29 @@
 
 // }
 
-// generatePassword()
+
+
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+let password = document.getElementById("result")
+
+
+
+
+function randomIndex() {
+    let number = Math.floor(Math.random() * characters.length)
+    return characters[number]
+}
+
+function generate() {
+    let generatedPassword = ""
+    for (let i = 0; i < 16; i++) {
+        generatedPassword += randomIndex()
+
+    }
+
+    password.innerText = generatedPassword
+}
+
+
